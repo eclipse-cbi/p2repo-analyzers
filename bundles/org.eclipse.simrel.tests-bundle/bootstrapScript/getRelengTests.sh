7320c21ba5b1cdd243cadc44a72eaf3d5c618b14
David Williams <david_williams@us.ibm.com>
2014-09-10 04:56:09 -0400

    [releng] adjust deployment for new repo structure

--- bundles/org.eclipse.simrel.tests-bundle/bootstrapScript/getRelengTests.sh
+++ bundles/org.eclipse.simrel.tests-bundle/bootstrapScript/getRelengTests.sh
@@ -9,7 +9,7 @@
 #    /shared/simrel/${release}/org.eclipse.simrel.tests
 # 2. copy "by hand" (or scp) this getRelengTests.sh file to /shared/simrel/${release} and run from hudson from there, the parent of 
 #    o.e.i.tests, namely "run shell script" /shared/simrel/${release}/getRelengTests.sh
-# 3. We currently assume "testInstance" already exists, as a child of /shared/simrel/${release}, and contains an instance of eclipse SDK (3.7). 
+# 3. We currently assume "testInstance" already exists, as a child of /shared/simrel/${release}, and (currently) contains an instance of eclipse SDK (4.3). 
 
 # if freshFlag is set, then "not freshFlag" is false and will skip 
 # the sanity check.   
@@ -28,7 +28,9 @@ fi
 source aggr_properties.shsource 2>/dev/null
 source ${BUILD_HOME}/org.eclipse.simrel.tests/aggr_properties.shsource
 
-RELENG_TESTS=${RELENG_TESTS:-org.eclipse.simrel.tests}
+RELENG_TESTS_REPO=${RELENG_TESTS_REPO:-org.eclipse.simrel.tests}
+RELENG_TESTS_PATH=${RELENG_TESTS_PATH:-bundles/org.eclipse.simrel.tests-bundle}
+RELENG_TESTS_DIR=${RELENG_TESTS_DIR:-org.eclipse.simrel.tests}
 BRANCH_TESTS=${BRANCH_TESTS:-master}
 TMPDIR_TESTS=${TMPDIR_TESTS:-sbtests}
 CGITURL=${CGITURL:-http://git.eclipse.org/c/simrel}
@@ -72,35 +74,41 @@ shift $(($OPTIND - 1))
 # if needed for debugging
 if $verboseFlag
 then
-    env
-    echo "fresh install: $freshFlag"
-    echo "verbose output: $verboseFlag"
-    echo "force clean prereqs: $cleanFlag"
-    echo "BUILD_TESTS: ${RELENG_TESTS}"
-    echo "TMPDIR_TESTS=${TMPDIR_TESTS}"
-
+    echo -e "\n\t == Verbose mode. =="
+    env > envSettings.txt
+    echo -e "\tSee 'envSettings.txt' for environment settings."
+    echo -e "\tfresh install: $freshFlag"
+    echo -e "\tverbose output: $verboseFlag"
+    echo -e "\tforce clean prereqs: $cleanFlag"
+    echo -e "\tRELENG_TESTS_REPO: ${RELENG_TESTS_REPO}"
+    echo -e "\tRELENG_TESTS_PATH: ${RELENG_TESTS_PATH}"
+    echo -e "\tRELENG_TESTS_DIR: ${RELENG_TESTS_DIR}"
+    echo -e "\tTMPDIR_TESTS=${TMPDIR_TESTS}"
+    echo -e "\t == == \n"
+else
+    echo -e "\t == Non-verbose mode. == "
 fi
 
 echo "CGITURL: ${CGITURL}"
-echo "BRANCH_TOOLS: ${BRANCH_TESTS}"
+echo "BRANCH_TESTS: ${BRANCH_TESTS}"
 
 # echo current directory
 echo "Current Directory: ${PWD}"
 
 # This script file is to help get builds started "fresh", when
-# the ${RELENG_TESTS} directory already exists on local file system.
-# While it is in the cvs repository in ${RELENG_TESTS}, it is
+# the ${RELENG_TESTS_REPO} directory already exists on local file system.
+# While it is in the cvs repository in ${RELENG_TESTS_REPO}, it is
 # meant to be executed from the parent directory
-# of ${RELENG_TESTS} on the file system.
+# of ${RELENG_TESTS_REPO} on the file system.
 
 # export is used, instead of checkout, just to avoid the CVS directories and since this code
 # for a local build, there should never be a need to check it back in to CVS.
 
 # if freshFlag is set, then "not freshFlag" is false and will skip 
 # the sanity check.   
-if ! $freshFlag && [[ ! -e ${RELENG_TESTS} ]]
+if ! $freshFlag && [[ ! -e ${RELENG_TESTS_DIR} ]]
 then
-    echo "${RELENG_TESTS} does not exist as sub directory";
+    echo "${RELENG_TESTS_DIR} does not exist as sub directory";
     usage
     exit 1;
 fi
@@ -108,29 +116,27 @@ fi
 
 # make sure RELENG_TESTS has been defined and is no zero length, or 
 # else following will eval to "rm -fr /*" ... potentially catastrophic
-if [ -z "${RELENG_TESTS}" ]
+if [ -z "${RELENG_TESTS_DIR}" ]
 then
-    echo "The variable RELENG_TESTS must be defined to run this script"
+    echo "The variable RELENG_TESTS_DIR must be defined to run this script"
     usage
     exit 1;
 fi
 
 
-echo "    removing all of ${RELENG_TESTS} ..."
-rm -fr "${RELENG_TESTS}"
-mkdir -p "${RELENG_TESTS}"
+echo "    removing all of ${RELENG_TESTS_DIR} ..."
+rm -fr "${RELENG_TESTS_DIR}"
+mkdir -p "${RELENG_TESTS_DIR}"
 
-
-
-# remove if already exists
-rm ${BRANCH_TESTS////_}.zip* 2>/dev/null
+# remove if already exists from previous run
+rm ${RELENG_TESTS_REPO////_}.zip* 2>/dev/null
 rm -fr ${TMPDIR_TESTS} 2>/dev/null 
 
-wget --no-verbose -O ${BRANCH_TESTS////_}.zip ${CGITURL}/${RELENG_TESTS}.git/snapshot/${BRANCH_TESTS}.zip 2>&1
+wget --no-verbose -O ${RELENG_TESTS_REPO////_}.zip ${CGITURL}/${RELENG_TESTS_REPO}.git/snapshot/${BRANCH_TESTS}.zip 2>&1
 RC=$?
 if [[ $RC != 0 ]] 
 then
-    echo "   ERROR: Failed to get ${BRANCH_TESTS////_}.zip from  ${CGITURL}/${BUILD_TESTS}/snapshot/${BRANCH_TESTS}.zip"
+    echo "   ERROR: Failed to get ${RELENG_TESTS_REPO////_}.zip from  ${CGITURL}/${RELENG_TESTS_REPO}/snapshot/${BRANCH_TESTS}.zip"
     echo "   RC: $RC"
     usage
     exit $RC
@@ -142,11 +148,11 @@ then
     quietZipFlag=
 fi
 
-unzip ${quietZipFlag} -o ${BRANCH_TESTS////_}.zip -d ${TMPDIR_TESTS} 
+unzip ${quietZipFlag} -o ${RELENG_TESTS_REPO////_}.zip -d ${TMPDIR_TESTS} 
 RC=$?
 if [[ $RC != 0 ]] 
 then
-    echo "/n/t%s/t%s/n" "ERROR:" "Failed to unzip ${BRANCH_TESTS////_}.zip to ${TMPDIR_TESTS}"
+    echo "/n/t%s/t%s/n" "ERROR:" "Failed to unzip ${RELENG_TESTS_REPO////_}.zip to ${TMPDIR_TESTS}"
     echo "   RC: $RC"
     usage
     exit $RC
@@ -158,11 +164,11 @@ then
     rsynchvFlag=-v
 fi
 
-rsync $rsynchvFlag -r ${TMPDIR_TESTS}/${BRANCH_TESTS}/ ${RELENG_TESTS}
+rsync $rsynchvFlag -r ${TMPDIR_TESTS}/${BRANCH_TESTS}/${RELENG_TESTS_PATH}/ ${RELENG_TESTS_DIR}/
 RC=$?
 if [[ $RC != 0 ]] 
 then
-    echo "ERROR: Failed to copy ${RELENG_TESTS} from ${TMPDIR_TESTS}/${BRANCH_TESTS}/"
+    echo "ERROR: Failed to copy ${RELENG_TESTS_DIR} from ${TMPDIR_TESTS}/${BRANCH_TESTS}/${RELENG_TESTS_PATH}/"
     echo "   RC: $RC"
     usage
     exit $RC
@@ -170,15 +176,15 @@ fi
 
 
 echo "    making sure releng control files are executable and have proper EOL ..."
-dos2unix ${RELENG_TESTS}/*.sh* ${RELENG_TESTS}/*.properties ${RELENG_TESTS}/*.xml >/dev/null 2>>/dev/null
-chmod +x ${RELENG_TESTS}/*.sh > /dev/null
+dos2unix ${RELENG_TESTS_DIR}/*.sh* ${RELENG_TESTS_DIR}/*.properties ${RELENG_TESTS_DIR}/*.xml >/dev/null 2>>/dev/null
+chmod +x ${RELENG_TESTS_DIR}/*.sh > /dev/null
 echo "    Done. "
 
 if $cleanFlag
 then
     # should very rarely need to do this, Like, once release. 
     # But Eclipse (OSGi?) creates some files with 
-    # only group read access, so to complete remove them, must use 
+    # only group read access, so to completly remove them, must use 
     # hudsonbuild ID to get completely clean. 
     echo "    removing all of testInstance directory"
     rm -fr testInstance
@@ -186,8 +192,8 @@ fi
 
 #if ! $verboseFlag
 #then
-# cleanup unless verbose/debugging
-rm ${BRANCH_TESTS////_}.zip* 
+## cleanup unless verbose/debugging
+rm ${RELENG_TESTS_REPO////_}.zip* 
 rm -fr ${TMPDIR_TESTS} 
 #fi
 
