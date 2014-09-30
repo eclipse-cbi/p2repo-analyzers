67986324d501d31840527116d92ec7121d578467
David Williams <david_williams@us.ibm.com>
2014-09-30 15:48:46 -0400

    [releng] correct 'verify' to 'validate'

--- bundles/org.eclipse.simrel.tests-bundle/ant.sh
+++ bundles/org.eclipse.simrel.tests-bundle/ant.sh
@@ -2,5 +2,13 @@
 
 source aggr_properties.shsource
 
+if [ -z $ANT_HOME ] ; then
+   echo "ANT_HOME must be defined to use this script.";
+   exit 1;
+fi
+
+echo "Starting ant via shell script"
+echo "ANT_HOME: " ${ANT_HOME}
+
 ANT_CMD="${ANT_HOME}/bin/ant"
-${ANT_HOME}/bin/ant "$@"
+${ANT_HOME}/bin/ant "$@" &
