235e607212a55dfce6337572e7c86e23f62cad3a
David Williams <david_williams@us.ibm.com>
2015-05-10 19:01:06 -0400

    Bug 466599 - Include (partial) path in Simultaneous release scan tool reports
    Change-Id: Iaac4cf17ab97a3845962de692bec1f4f20895421
    Signed-off-by: David Williams <david_williams@us.ibm.com>

--- bundles/org.eclipse.simrel.tests-bundle/verify.sh
+++ bundles/org.eclipse.simrel.tests-bundle/verify.sh
@@ -2,14 +2,14 @@
 
 if [[ $# != 2 && $# != 3 ]] 
 then
-    printf "\tERROR: Wrong number of arguments. This script, "${0##*/}", requires the name of the jar or jar.pack.gz file to verify and the version of Java to use 8,7,6, or 5, and optionally the location scanned.\n" >&2
-    exit 1
+  printf "\tERROR: Wrong number of arguments. This script, "${0##*/}", requires the name of the jar or jar.pack.gz file to verify and the version of Java to use 8,7,6, or 5, and optionally the location scanned.\n" >&2
+  exit 1
 fi
 
 if [[ -z $VERIFYOUTDIR ]] 
 then
-    printf "\tERROR: this script, "${0##*/}", requires VERIFYOUTDIR to be defined.\n" >&2
-    exit 1
+  printf "\tERROR: this script, "${0##*/}", requires VERIFYOUTDIR to be defined.\n" >&2
+  exit 1
 fi
 
 
@@ -18,15 +18,15 @@ filename="${1}"
 
 if [ ! -e "${filename}" ]
 then
-    printf "\tERROR: file does not exist:" "${filename}\n" >&2
-    exit 1
+  printf "\tERROR: file does not exist:" "${filename}\n" >&2
+  exit 1
 fi
 
 JAVA_VER=$2
 
 if [[ ! "${JAVA_VER}" =~ [5,6,7,8] ]]
 then
-    printf "\tERROR: The second argumnet to this script,  "${0##*/}", must be a Java Version numerial, from 5 to 8\n" >&2
+  printf "\tERROR: The second argumnet to this script,  "${0##*/}", must be a Java Version numerial, from 5 to 8\n" >&2
 fi
 
 #Location is optional. Just used to write in info file.
@@ -40,19 +40,19 @@ JAVA_7_HOME=/shared/common/jdk1.7.0-latest
 JAVA_8_HOME=/shared/common/jdk1.8.0_x64-latest
 
 case $JAVA_VER in
-    5) 
+  5) 
     JAVA_HOME=$JAVA_5_HOME
     ;;
-    6)
+  6)
     JAVA_HOME=$JAVA_6_HOME
     ;;
-    7)
+  7)
     JAVA_HOME=$JAVA_7_HOME
     ;;
-    8)
+  8)
     JAVA_HOME=$JAVA_8_HOME
     ;;
-    *)
+  *)
     printf "\n\tPROGRAM ERROR: \t%s\n" "JAVA_VER, ${JAVA_VER}, was not valid (which was checked earlier in regex expression)"
     ;;
 esac
@@ -62,73 +62,74 @@ INFO_FILE="${VERIFYOUTDIR}/${INFO_FILENAME}"
 # if already exists, we don't need to re-write
 if [[ ! -e "${INFO_FILE}" ]]
 then
-    printf "JAVA_HOME: \t%s\n\n" "${JAVA_HOME}" > "${INFO_FILE}"
-    printf "Java version: \t%s\n\n" "$( ${JAVA_HOME}/bin/java -version 2>&1 )" >> "${INFO_FILE}"
-    printf "verify script: \t%s\n\n" "${0}" >> "${INFO_FILE}"
-    printf "location scanned: \t${LOCATION}" >> "${INFO_FILE}" 
+  printf "JAVA_HOME: \t%s\n\n" "${JAVA_HOME}" > "${INFO_FILE}"
+  printf "Java version: \t%s\n\n" "$( ${JAVA_HOME}/bin/java -version 2>&1 )" >> "${INFO_FILE}"
+  printf "verify script: \t%s\n\n" "${0}" >> "${INFO_FILE}"
+  printf "location scanned: \t${LOCATION}" >> "${INFO_FILE}" 
 fi 
 
 if [ ! -e "${JAVA_HOME}" ]
 then
-    printf "\tERROR: this script, "${0##*/}", requires JAVA_HOME to be defined and exist.\n" >&2
-    exit 1
+  printf "\tERROR: this script, "${0##*/}", requires JAVA_HOME to be defined and exist.\n" >&2
+  exit 1
 fi
 
 UNPACK200_EXE=$JAVA_HOME/jre/bin/unpack200
 VERIFY_EXE=$JAVA_HOME/bin/jarsigner
-#VERIFY_OPTIONS="-verify -verbose  -certs"
-#COMPACT=false
-#VERIFY_OPTIONS="-verify -verbose"
-VERIFY_OPTIONS="-verify"
-COMPACT=true
+#VERIFY_OPTIONS=${VERIFY_OPTIONS:-"-verify -verbose  -certs"
+#COMPACT=${COMPACT:-false}
+#VERIFY_OPTIONS=${VERIFY_OPTIONS:-"-verify -verbose"}
+#COMPACT=${COMPACT:-false}
+VERIFY_OPTIONS=${VERIFY_OPTIONS:-"-verify"}
+COMPACT=${COMPACT:-true}
 
-CHECK_SUM_FILE="${VERIFYOUTDIR}/checksums${JAVA_VER}.txt"
-VERIFIED_OUTFILE="${VERIFYOUTDIR}/verified${JAVA_VER}.txt"
-KNOWN_EXCEPTION="${VERIFYOUTDIR}/knownunsigned${JAVA_VER}.txt"
-UNSIGNED_OUTFILE="${VERIFYOUTDIR}/unsigned${JAVA_VER}.txt"
-NOMANIFEST="${VERIFYOUTDIR}/nomanifest${JAVA_VER}.txt"
-ERROR_EXIT_FILE="${VERIFYOUTDIR}/error${JAVA_VER}.txt"
-NESTED_JARS="${VERIFYOUTDIR}/nestedjars${JAVA_VER}.txt"
-TMP_DIR=${TMP_DIR:-/shared/orbit/tmp/}
+CHECK_SUM_FILE=${CHECK_SUM_FILE:-"${VERIFYOUTDIR}/checksums${JAVA_VER}.txt"}
+VERIFIED_OUTFILE=${VERIFIED_OUTFILE:-"${VERIFYOUTDIR}/verified${JAVA_VER}.txt"}
+KNOWN_EXCEPTION=${KNOWN_EXCEPTION:-"${VERIFYOUTDIR}/knownunsigned${JAVA_VER}.txt"}
+UNSIGNED_OUTFILE=${UNSIGNED_OUTFILE:-"${VERIFYOUTDIR}/unsigned${JAVA_VER}.txt"}
+NOMANIFEST=${NOMANIFEST:-"${VERIFYOUTDIR}/nomanifest${JAVA_VER}.txt"}
+ERROR_EXIT_FILE=${ERROR_EXIT_FILE:-"${VERIFYOUTDIR}/error${JAVA_VER}.txt"}
+NESTED_JARS=${NESTED_JARS:-"${VERIFYOUTDIR}/nestedjars${JAVA_VER}.txt"}
+TMP_DIR=${TMP_DIR:-/shared/simrel/tmp/}
 mkdir -p ${TMP_DIR}
 PPAT_PACKGZ="(.*).pack.gz$"
 if [[ "$jarname" =~  $PPAT_PACKGZ ]]
 then 
-    basejarname=${BASH_REMATCH[1]}
-    #echo -e "\n basejarname: " $basejarname "\n"
-    "${UNPACK200_EXE}" $filename ${TMP_DIR}/$basejarname
-    vresult=`"${VERIFY_EXE}" ${VERIFY_OPTIONS} ${TMP_DIR}/$basejarname`
-    exitcode=$?
-    #nestedPackedJars=$( ${JAVA_HOME}/bin/jar -t ${TMP_DIR}/$basejarname | grep "pack.gz" )
-    nestedPackedJars=$(  unzip -t ${TMP_DIR}/$basejarname 2>/dev/null | grep "pack.gz" )
-    if [[ -n $nestedPackedJars ]]
-    then
-        echo "$filename contains nested packed jars" >> ${NESTED_JARS}
-        echo "$nestedPackedJars" >> ${NESTED_JARS}
-    fi
-    csum=$(md5sum "${TMP_DIR}/${basejarname}")
-    # echo -e "$basejarname  \t${csum%% *}  \tjar.pack.gz" >> "${CHECK_SUM_FILE}"
-    printable_basejarname=$basejarname
-    printable_checksum=${csum%% *}
-    printable_type=jar.pack.gz
-    rm ${TMP_DIR}/$basejarname
+  basejarname=${BASH_REMATCH[1]}
+  #echo -e "\n basejarname: " $basejarname "\n"
+  "${UNPACK200_EXE}" $filename ${TMP_DIR}/$basejarname
+  vresult=`"${VERIFY_EXE}" ${VERIFY_OPTIONS} ${TMP_DIR}/$basejarname`
+  exitcode=$?
+  #nestedPackedJars=$( ${JAVA_HOME}/bin/jar -t ${TMP_DIR}/$basejarname | grep "pack.gz" )
+  nestedPackedJars=$(  unzip -t ${TMP_DIR}/$basejarname 2>/dev/null | grep "pack.gz" )
+  if [[ -n $nestedPackedJars ]]
+  then
+    echo "$filename contains nested packed jars" >> ${NESTED_JARS}
+    echo "$nestedPackedJars" >> ${NESTED_JARS}
+  fi
+  csum=$(md5sum "${TMP_DIR}/${basejarname}")
+  # echo -e "$basejarname  \t${csum%% *}  \tjar.pack.gz" >> "${CHECK_SUM_FILE}"
+  printable_basejarname=$basejarname
+  printable_checksum=${csum%% *}
+  printable_type=jar.pack.gz
+  rm ${TMP_DIR}/$basejarname
 else
-    vresult=`"${VERIFY_EXE}" ${VERIFY_OPTIONS} $filename`
-    exitcode=$?
-    csum=$(md5sum "$filename")
-    # echo -e "${filename##*/} \t${csum%% *} \tjar" >> "${CHECK_SUM_FILE}"
-    printable_basejarname=${filename##*/}
-    printable_checksum=${csum%% *}
-    printable_type=jar
+  vresult=`"${VERIFY_EXE}" ${VERIFY_OPTIONS} $filename`
+  exitcode=$?
+  csum=$(md5sum "$filename")
+  # echo -e "${filename##*/} \t${csum%% *} \tjar" >> "${CHECK_SUM_FILE}"
+  printable_basejarname=${filename##*/}
+  printable_checksum=${csum%% *}
+  printable_type=jar
 fi
 
 # jarsigner sometimes returns one line, sometimes two ... we take 
 # out EOLs to print compactly
 if [[ "true" == "${COMPACT}" ]]
 then
-    vresultoneline=$(echo "${vresult}" | tr '\n' ' ' | tr '\r' ' ')
+  vresultoneline=$(echo "${vresult}" | tr '\n' ' ' | tr '\r' ' ')
 else
-    vresultoneline="${vresult}"
+  vresultoneline="${vresult}"
 fi
 #vresultoneline=$(echo "${vresult}" | tr '\n' ' ' | tr '\r' ' ')
 # known response patterns from jarsigner (assumes EOLs removed from string)
@@ -144,63 +145,53 @@ PPAT_UNSIGNED_OR_NOMANIFEST="^.*(jar is unsigned)|(no manifest).*"
 JAR_TYPE="unknown"
 if [[ "${filename}" =~ .*plugins/.* ]]
 then
-    JAR_TYPE="plugin"
+  JAR_TYPE="plugin"
 elif [[ "${filename}" =~ .*features/.* ]]
 then
-    JAR_TYPE="features"
+  JAR_TYPE="features"
 else
-    JAR_TYPE="loose jar?" 
+  JAR_TYPE="loose jar?" 
 fi
 
 
 if [[ "${vresultoneline}" =~ $PPAT_VERIFIED ]]
 then
-    printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${VERIFIED_OUTFILE}" 
-    printf '%s\n' " ${vresultoneline} " >> "${VERIFIED_OUTFILE}"
-    # write checksums for verified files
-    printf "%-90s %30s %-15s\n" "${printable_basejarname}" $printable_checksum $printable_type >> "${CHECK_SUM_FILE}"
+  printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${VERIFIED_OUTFILE}" 
+  printf '%s\n' " ${vresultoneline} " >> "${VERIFIED_OUTFILE}"
+  # write checksums for verified files
+  printf "%-90s %30s %-15s\n" "${printable_basejarname}" $printable_checksum $printable_type >> "${CHECK_SUM_FILE}"
 elif [[ "${vresultoneline}" =~ $PPAT_UNSIGNED_OR_NOMANIFEST ]]
 then
-    # list "known cases", that can not be signed, 
-    # in their own "known exception file", else list in "unsigned" file.
+  # list "known cases", that can not be signed, 
+  # in their own "known exception file", else list in "unsigned" file.
 
-    # For reasons of addition of org.eclipse.jdt.core.compiler.batch see
-    # https://bugs.eclipse.org/bugs/show_bug.cgi?id=356382
-    # For small pointer about commonj.sdo, see
-    # https://bugs.eclipse.org/bugs/show_bug.cgi?id=276999
-    PPAT_COMMON_UNSIGNED='(artifacts\.jar)|(content\.jar)|(compositeArtifacts.jar)|(compositeContent.jar)'
-    PPAT_ORBIT_UNSIGNED='(commonj\.sdo.*)'
-    PPAT_ECLIPSE_UNSIGNED='(org\.eclipse\.jdt\.core\.compiler\.batch.*)|(org\.eclipse\.jdt\.core\.compiler\.batch\.source_.*)'
-    PPAT_KNOWN_UNSIGNED="$PPAT_COMMON_UNSIGNED|$PPAT_ORBIT_UNSIGNED|$PPAT_ECLIPSE_UNSIGNED"
-    echo "INFO: KNOWN UNSIGNED: $PPAT_KNOWN_UNSIGNED"
-    if  [[ ${jarname} =~ $PPAT_KNOWN_UNSIGNED ]]       
-    then
-        printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${KNOWN_EXCEPTION}"
-        printf '%s\n' " ${vresultoneline} "  >> "${KNOWN_EXCEPTION}"
+  # For reasons of addition of org.eclipse.jdt.core.compiler.batch see
+  # https://bugs.eclipse.org/bugs/show_bug.cgi?id=356382
+  # For small pointer about commonj.sdo, see
+  # https://bugs.eclipse.org/bugs/show_bug.cgi?id=276999
+  PPAT_COMMON_UNSIGNED='(artifacts\.jar)|(content\.jar)|(compositeArtifacts.jar)|(compositeContent.jar)'
+  PPAT_ORBIT_UNSIGNED='(commonj\.sdo.*)'
+  PPAT_ECLIPSE_UNSIGNED='(org\.eclipse\.jdt\.core\.compiler\.batch.*)|(org\.eclipse\.jdt\.core\.compiler\.batch\.source_.*)'
+  PPAT_KNOWN_UNSIGNED="$PPAT_COMMON_UNSIGNED|$PPAT_ORBIT_UNSIGNED|$PPAT_ECLIPSE_UNSIGNED"
+  echo "INFO: KNOWN UNSIGNED: $PPAT_KNOWN_UNSIGNED"
+  if  [[ ${jarname} =~ $PPAT_KNOWN_UNSIGNED ]]       
+  then
+    printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${KNOWN_EXCEPTION}"
+    printf '%s\n' " ${vresultoneline} "  >> "${KNOWN_EXCEPTION}"
 
-    else
-        # purposely no line delimiter, so output of jarsigner is on same line
-        printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${UNSIGNED_OUTFILE}" 
-        printf '%s\n' " ${vresultoneline} "  >> "${UNSIGNED_OUTFILE}"
-    fi 
+  else
+    # purposely no line delimiter, so output of jarsigner is on same line
+    printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${UNSIGNED_OUTFILE}" 
+    printf '%s\n' " ${vresultoneline} "  >> "${UNSIGNED_OUTFILE}"
+  fi 
 else 
-    # fall through if unexpected result. Will happen if can not unpack200 a file
-    printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${ERROR_EXIT_FILE}" 
-    printf '%s\n' " ${vresultpneline} "  >> "${ERROR_EXIT_FILE}" 
-fi
-
-# When 'compact' thing sort well, but, not if non-compact output.
-if [[ "true" == "${COMPACT}" ]] 
-then
-
-sort "${VERIFIED_OUTFILE}"
-sort "${KNOWN_EXCEPTION}"
-sort "${UNSIGNED_OUTFILE}"
-
+  # fall through if unexpected result. Will happen if can not unpack200 a file
+  printf '%-80s \t%-15s \t\t' "${jarname}" "$JAR_TYPE" >> "${ERROR_EXIT_FILE}" 
+  printf '%s\n' " ${vresultpneline} "  >> "${ERROR_EXIT_FILE}" 
 fi
 
 if [[ $exitcode -gt 0 ]]
 then
-    echo -e "\n exitcode: " $exitcode: $(basename $filename)" \n"  >> "${ERROR_EXIT_FILE}"  
+  echo -e "\n exitcode: " $exitcode: $(basename $filename)" \n"  >> "${ERROR_EXIT_FILE}"  
 fi
 
--- bundles/org.eclipse.simrel.tests-bundle/verifydir.sh
+++ bundles/org.eclipse.simrel.tests-bundle/verifydir.sh
@@ -7,26 +7,26 @@ EXECDIR=${0%/*}
 
 function info () 
 {
-    echo -e "\n\tverify directory: ${loc}"
-    echo -e "\n\tfor file pattern: ${pat}"
-    echo -e "\n\tresult list files are in \n${VERIFYOUTDIR}\n"
+  echo -e "\n\tverify directory: ${loc}"
+  echo -e "\n\tfor file pattern: ${pat}"
+  echo -e "\n\tresult list files are in \n${VERIFYOUTDIR}\n"
 }
 
 function pause () 
 {
-    TIMEOUT=$1
-    if [[ -z ${TIMEOUT} ]]  
-    then
-        TIMEOUT=10
-    fi
-    echo -e "\n"
-    read -t$TIMEOUT -n1 -r -p "Result list files will be in ${VERIFYOUTDIR} (and any existing files there removed) unless ctrl-c pressed before 5 seconds ..." key
-    echo -e "\n"
-    if [ $? -eq 0 ]; then
-        return 0; #echo A key was pressedi: $key.
-    else
-        return 1; #echo No key was pressed.
-    fi
+  TIMEOUT=$1
+  if [[ -z ${TIMEOUT} ]]  
+  then
+    TIMEOUT=10
+  fi
+  echo -e "\n"
+  read -t$TIMEOUT -n1 -r -p "Result list files will be in ${VERIFYOUTDIR} (and any existing files there removed) unless ctrl-c pressed before 5 seconds ..." key
+  echo -e "\n"
+  if [ $? -eq 0 ]; then
+    return 0; #echo A key was pressedi: $key.
+  else
+    return 1; #echo No key was pressed.
+  fi
 }
 
 
@@ -37,65 +37,94 @@ echo -e "\ncommand line file pattern to match:  \t$3\n"
 
 if [ -z "${1}" ]
 then
-    loc=${PWD}
+  loc=${PWD}
 else
-    loc=${1}
+  loc=${1}
 fi
 
 if [ -z $2 ]
 then
-    export VERIFYOUTDIR="${HOME}"/verifydiroutput
+  export VERIFYOUTDIR="${HOME}"/verifydiroutput
 else
-    export VERIFYOUTDIR="${2}"/verifydiroutput
+  export VERIFYOUTDIR="${2}"/verifydiroutput
 fi
 
 if [ -z $3 ]
 then
-    pat=".*\(\.jar$\|\.jar\.pack\.gz$\)"
+  pat=".*\(\.jar$\|\.jar\.pack\.gz$\)"
 else
-    pat="${3}"
+  pat="${3}"
 fi
 
 # VERIFYOUTDIR is used in 'verify.sh' and must be clean before
 # beginning the overall check
 if [[ -e "${VERIFYOUTDIR}" ]]
 then
-    pause
-    rm -fr "${VERIFYOUTDIR}"
+  pause
+  rm -fr "${VERIFYOUTDIR}"
 fi
 
 # make, in case doesn't exist
 mkdir -p "${VERIFYOUTDIR}"
 
+UNPACK200_EXE=$JAVA_HOME/jre/bin/unpack200
+VERIFY_EXE=$JAVA_HOME/bin/jarsigner
+#VERIFY_OPTIONS=${VERIFY_OPTIONS:-"-verify -verbose  -certs"
+#COMPACT=${COMPACT:-false}
+#VERIFY_OPTIONS=${VERIFY_OPTIONS:-"-verify -verbose"}
+#COMPACT=${COMPACT:-false}
+VERIFY_OPTIONS=${VERIFY_OPTIONS:-"-verify"}
+COMPACT=${COMPACT:-true}
+
 #TMP_DIR is used by verify.sh when it unpack's things
 now=$( date --utc +%s )
-export TMP_DIR=${TMP_DIR:-/shared/orbit/tmp/$(date --utc -d @$now +%Y-%m%d_%H%M-%S)}
+export TMP_DIR=${TMP_DIR:-/shared/simrel/tmp/$(date --utc -d @$now +%Y-%m%d_%H%M-%S)}
 
 # We display general info at beginning, and at end, just so it is easier not to miss.
 info
 # 6 7 
 for ver in 8 
 do
-    find "${loc}" -regex "${pat}" -exec ${EXECDIR}/verify.sh '{}' $ver "${loc}" \;
+  # These are needed by verify.sh, but we define them here, so we can sort 
+  # them just once, at end.
+  CHECK_SUM_FILE="${VERIFYOUTDIR}/checksums${ver}.txt"
+  VERIFIED_OUTFILE="${VERIFYOUTDIR}/verified${ver}.txt"
+  KNOWN_EXCEPTION="${VERIFYOUTDIR}/knownunsigned${ver}.txt"
+  UNSIGNED_OUTFILE="${VERIFYOUTDIR}/unsigned${ver}.txt"
+  NOMANIFEST="${VERIFYOUTDIR}/nomanifest${ver}.txt"
+  ERROR_EXIT_FILE="${VERIFYOUTDIR}/error${ver}.txt"
+  NESTED_JARS="${VERIFYOUTDIR}/nestedjars${ver}.txt"
+
+  find "${loc}" -regex "${pat}" -exec ${EXECDIR}/verify.sh '{}' $ver "${loc}" \;
 done
 
 info
 
-if [[ -e  ${VERIFYOUTDIR}/checksums8.txt ]]
+if [[ -e  ${VERIFYOUTDIR}/checksums${ver}.txt ]]
 then
-    sort  -k1.1,90 -k3.1,10 -r < ${VERIFYOUTDIR}/checksums8.txt > ${VERIFYOUTDIR}/checksums-sorted8.txt
+  sort  -k1.1,90 -k3.1,10 -r < ${VERIFYOUTDIR}/checksums${ver}.txt > ${VERIFYOUTDIR}/checksums${ver}-sorted.txt
 fi
-if [[ -e  ${VERIFYOUTDIR}/checksums7.txt ]]
+# When 'compact' thing sort well, but, not if non-compact output.
+if [[ "true" == "${COMPACT}" ]] 
 then
-    sort  -k1.1,90 -k3.1,10 -r < ${VERIFYOUTDIR}/checksums7.txt > ${VERIFYOUTDIR}/checksums-sorted7.txt
+
+  if [[ -e "${VERIFIED_OUTFILE}" ]]
+  then
+    sort -o "${VERIFIED_OUTFILE}" "${VERIFIED_OUTFILE}"
+  fi
+  if [[ -e "${UNSIGNED_OUTFILE}" ]]
+  then
+    sort -o "${UNSIGNED_OUTFILE}" "${UNSIGNED_OUTFILE}"
+  fi
+  if [[ -e "${KNOWN_EXCEPTION}" ]]
+  then
+    sort -o "${KNOWN_EXCEPTION}" "${KNOWN_EXCEPTION}"
+  fi
+  if [[ -e "${NOMANIFEST}" ]]
+  then
+    sort -o "${NOMANIFEST}" "${NOMANIFEST}"
+  fi
 fi
-if [[ -e  ${VERIFYOUTDIR}/checksums6.txt ]]
-then
-    sort  -k1.1,90 -k3.1,10 -r < ${VERIFYOUTDIR}/checksums6.txt > ${VERIFYOUTDIR}/checksums-sorted6.txt
-fi
-
-
-
 
 # if certain files were not created during signing checks, create a "plain" one at expected location, 
 # to avoid 404 errors from standard index.html. For example, all jars are signed, there will be no unsigned.txt files.
