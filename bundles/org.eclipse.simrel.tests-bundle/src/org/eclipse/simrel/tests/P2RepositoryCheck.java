3c2a7cac7fac7f605922f44184a6f4041f9e61e7
Dennis Huebner <dennis.huebner@itemis.de>
2015-05-21 16:34:42 +0200

    [common] Do not mark application as failed if there is an error report
    Signed-off-by: Dennis Huebner <dennis.huebner@itemis.de>

--- bundles/org.eclipse.simrel.tests-bundle/src/org/eclipse/simrel/tests/P2RepositoryCheck.java
+++ bundles/org.eclipse.simrel.tests-bundle/src/org/eclipse/simrel/tests/P2RepositoryCheck.java
@@ -9,7 +9,6 @@ import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.equinox.p2.core.ProvisionException;
 import org.eclipse.simrel.tests.common.P2RepositoryAnalyser;
 import org.eclipse.simrel.tests.common.P2RepositoryDescription;
-import org.eclipse.simrel.tests.common.ReportType;
 import org.eclipse.simrel.tests.common.checker.CheckerRegistry;
 import org.eclipse.simrel.tests.common.reporter.CheckReportsManager;
 import org.eclipse.simrel.tests.common.reporter.ICheckReporter;
@@ -27,25 +26,29 @@ import com.google.common.collect.Lists;
 public class P2RepositoryCheck {
     private List<ICheckReporter> reporter = Lists.newArrayList(new JunitXmlReport(), new OverviewReport());
 
-    public boolean runChecks(RepoTestsConfiguration configuration) throws ProvisionException, OperationCanceledException {
+    public boolean runChecks(RepoTestsConfiguration configuration) {
         Stopwatch stopwatch = Stopwatch.createStarted();
+        try {
+            P2RepositoryDescription p2Repo = IUUtil.createRepositoryDescription(configuration.getReportRepoURI());
+            System.out.println("create repo descr: " + stopwatch);
+            stopwatch.reset().start();
 
-        P2RepositoryDescription p2Repo = IUUtil.createRepositoryDescription(configuration.getReportRepoURI());
-        System.out.println("create repo descr: " + stopwatch);
-        stopwatch.reset().start();
-
-        CheckerRegistry registry = new CheckerRegistry();
-        P2RepositoryAnalyser analyser = new P2RepositoryAnalyser();
-        CheckReportsManager manager = new CheckReportsManager();
-        analyser.analyse(p2Repo, registry, manager);
-        System.out.println("run analyse: " + stopwatch);
-        stopwatch.reset().start();
-        for (ICheckReporter iCheckReporter : reporter) {
-            iCheckReporter.createReport(manager, configuration);
+            CheckerRegistry registry = new CheckerRegistry();
+            P2RepositoryAnalyser analyser = new P2RepositoryAnalyser();
+            CheckReportsManager manager = new CheckReportsManager();
+            analyser.analyse(p2Repo, registry, manager);
+            System.out.println("run analyse: " + stopwatch);
+            stopwatch.reset().start();
+            for (ICheckReporter iCheckReporter : reporter) {
+                iCheckReporter.createReport(manager, configuration);
+            }
+            System.out.println("dump reports: " + stopwatch);
+            System.out.println("Reports output: " + configuration.getReportOutputDir());
+            stopwatch.stop();
+        } catch (ProvisionException | OperationCanceledException e) {
+            e.printStackTrace();
+            return true;
         }
-        System.out.println("dump reports: " + stopwatch);
-        System.out.println("Reports output: " + configuration.getReportOutputDir());
-        stopwatch.stop();
-        return manager.getReports().stream().anyMatch(report -> report.getType() == ReportType.NOT_IN_TRAIN);
+        return false;
     }
 }
