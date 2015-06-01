9ca3ff00b0a95b1edc8136118cc78b10789e030f
David Williams <david_williams@us.ibm.com>
2015-06-01 02:38:35 -0400

    Bug 467449 - [reports] Build "repoReports" tools into product and repository
    tweaks to branch to fix URLs

--- bundles/org.eclipse.simrel.tests-bundle/verifydir.sh
+++ bundles/org.eclipse.simrel.tests-bundle/verifydir.sh
@@ -130,15 +130,15 @@ fi
 # to avoid 404 errors from standard index.html. For example, all jars are signed, there will be no unsigned.txt files.
 # TODO: change HTML to PHP, so we can "check for the existence" of these files ... i.e. easer to flag 
 # an error if exists or not, instead of based on size? 
-if [ ! -e "${VERIFYOUTDIR}"/unsigned8.txt ]
+if [ ! -e "${VERIFYOUTDIR}"/unsigned${ver}.txt ]
 then
     echo  "There were no unsigned jars in the directories checked. " > "${VERIFYOUTDIR}"/unsigned${ver}.txt
 fi 
-if [ ! -e "${VERIFYOUTDIR}"/errors8.txt ]
+if [ ! -e "${VERIFYOUTDIR}"/errors${ver}.txt ]
 then
     echo  "There were no unsigned jars in the directories checked. " > "${VERIFYOUTDIR}"/unsigned${ver}.txt
 fi 
-if [ ! -e "${VERIFYOUTDIR}"/nestedjars8.txt ]
+if [ ! -e "${VERIFYOUTDIR}"/nestedjars${ver}.txt ]
 then
     echo  "There were no nested packed jars in the directories checked. " > "${VERIFYOUTDIR}"/nestedjars${ver}.txt
 fi 
