6b3d8604db66e3486ed639497e69c87e0c0e9ebb
David Williams <david_williams@us.ibm.com>
2015-06-03 14:32:29 -0400

    [releng] Found error. Typo. Was overwriting "unsigned" file after checking for existence of "error" file (instead of error file).

--- bundles/org.eclipse.simrel.tests-bundle/verifydir.sh
+++ bundles/org.eclipse.simrel.tests-bundle/verifydir.sh
@@ -130,13 +130,13 @@ fi
 # to avoid 404 errors from standard index.html. For example, all jars are signed, there will be no unsigned.txt files.
 # TODO: change HTML to PHP, so we can "check for the existence" of these files ... i.e. easer to flag 
 # an error if exists or not, instead of based on size? 
-if [ ! -e "${VERIFYOUTDIR}"/unsigned${ver}.txt ]
+if [ ! -e "${UNSIGNED_OUTFILE} ]
 then
-    echo  "There were no unsigned jars in the directories checked. " > "${VERIFYOUTDIR}"/unsigned${ver}.txt
+    echo  "There were no unsigned jars in the directories checked. " > "${UNSIGNED_OUTFILE}"
 fi 
 if [ ! -e "${VERIFYOUTDIR}"/errors${ver}.txt ]
 then
-    echo  "There were no unsigned jars in the directories checked. " > "${VERIFYOUTDIR}"/unsigned${ver}.txt
+    echo  "There were no verify error jars in the directories checked. " > "${VERIFYOUTDIR}"/errors${ver}.txt
 fi 
 if [ ! -e "${VERIFYOUTDIR}"/nestedjars${ver}.txt ]
 then
