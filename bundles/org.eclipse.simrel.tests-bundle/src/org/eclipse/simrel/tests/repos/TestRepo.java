24cff9fdb2d0cdafa76b69b36186aa3ec89818e5
Dennis Huebner <dennis.huebner@itemis.de>
2015-07-10 17:16:48 +0200

    Added further checks to exclude license feature from test
    Signed-off-by: Dennis Huebner <dennis.huebner@itemis.de>

--- bundles/org.eclipse.simrel.tests-bundle/src/org/eclipse/simrel/tests/repos/TestRepo.java
+++ bundles/org.eclipse.simrel.tests-bundle/src/org/eclipse/simrel/tests/repos/TestRepo.java
@@ -105,11 +105,12 @@ public class TestRepo extends BuildRepoTests {
     }
     
     /**
+     * In the shared eclipse license, the provider- and feature- name are <code>null</code> 
      * @param iu Unit to check
      * @return <code>true</code> if the <code>iu</code> an eclipse shared license feature
      */
-    protected boolean isEclipseLicenseFeature(IInstallableUnit iu) {
-        return "org.eclipse.license.feature.group".equals(iu.getId());
+    protected boolean isEclipseLicenseIU(IInstallableUnit iu) {
+        return "org.eclipse.license.feature.group".equals(iu.getId()) || "org.eclipse.license.feature.jar".equals(iu.getId());
     }
     
 
