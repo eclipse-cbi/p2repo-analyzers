d7af0dba7ca57b0b652f908166e8acf1b5f823f1
Dennis Huebner <dennis.huebner@gmail.com>
2016-11-24 11:12:30 +0100

    [ui] Analyzer eclipse plugin - initial
    Change-Id: I66b1568bbf2c9b357337009bf57bb5a4a5c1fdb7
    Signed-off-by: Dennis Huebner <dennis.huebner@gmail.com>

---
+++ bundles/org.eclipse.cbi.p2repo.analyzers.ui/src/org/eclipse/cbi/p2repo/analyzers/ui/internal/AnalyzerActivator.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2016 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.cbi.p2repo.analyzers.ui.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author dhuebner - Initial contribution and API
+ */
+public class AnalyzerActivator implements BundleActivator {
+
+	/* (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bundles/org.eclipse.cbi.p2repo.analyzers.ui/src/org/eclipse/cbi/p2repo/analyzers/ui/popup/actions/P2AnalyzerAction.java
@@ -0,0 +1,75 @@
+package org.eclipse.cbi.p2repo.analyzers.ui.popup.actions;
+
+import org.eclipse.cbi.p2repo.analyzers.P2RepositoryCheck;
+import org.eclipse.cbi.p2repo.analyzers.RepoTestsConfiguration;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.ide.IDE;
+
+public class P2AnalyzerAction implements IObjectActionDelegate {
+
+	private ISelection selection;
+	private IWorkbenchPart targetPart;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.
+	 * action.IAction, org.eclipse.ui.IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		this.targetPart = targetPart;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	public void run(IAction action) {
+		if (this.selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) this.selection;
+			Object first = structuredSelection.getFirstElement();
+			if (first instanceof IFolder) {
+				IFolder iFolder = (IFolder) first;
+				String reportRepoDir = iFolder.getLocation().toOSString();
+				IFolder outputFolder = iFolder.getParent().getFolder(new Path("p2-report")); //$NON-NLS-1$
+				RepoTestsConfiguration configurations = new RepoTestsConfiguration(reportRepoDir,
+						outputFolder.getLocation().toOSString(), null, System.getProperty("java.io.tmpdir")); //$NON-NLS-1$
+				new P2RepositoryCheck().runChecks(configurations);
+				IFile htmlIFile = outputFolder.getFile("errors-and-moderate_warnings.html"); //$NON-NLS-1$
+				try {
+					iFolder.getParent().refreshLocal(1, new NullProgressMonitor());
+					MessageDialog.openInformation(targetPart.getSite().getShell(), Messages.P2AnalyzerAction_dialog_info_title,
+							Messages.P2AnalyzerAction_dialog_info_msg + outputFolder.getFullPath());
+					IDE.openEditor(targetPart.getSite().getWorkbenchWindow().getActivePage(), htmlIFile);
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.
+	 * IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+	}
+
+}
