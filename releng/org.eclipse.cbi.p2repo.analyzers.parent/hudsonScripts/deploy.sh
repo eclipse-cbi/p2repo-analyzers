ae02ac4cc49280a2d3c96531e6d26ac8e8d84a86
David Williams <david_williams@acm.org>
2016-12-01 01:44:48 -0500

    Bug 507917 - Convert cbi.p2repo.analyzers.build
    Change-Id: Ia6ece44cc688b784d3522fb9c7476ce8c7041508
    Signed-off-by: David Williams <david_williams@acm.org>

---
+++ releng/org.eclipse.cbi.p2repo.analyzers.parent/hudsonScripts/deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# This job is ran after "runTychoBuild.sh" is ran in the "cleanAndDeploy" Hudson job.
+
+# The default for build home is simply where I do my local build. Feel free to change in your local copy. 
+build_home=${WORKSPACE:-/home/davidw/gitCBI}
+propertiesfile="${build_home}/org.eclipse.cbi.p2repo.aggregator/org.eclipse.cbi.p2repo.cli.product/target/mavenproperties.properties"
+sourceProperties="${build_home}/mavenproperties.shsource"
+phpProperties="${build_home}/mavenproperties.php"
+
+function deployRepos
+{
+  newRepo=$1
+  builtRepo=$2
+  mkdir -p ${newRepo}
+  cp -r ${builtRepo}/* ${newRepo}/
+
+  #TODO: add mirror URL, etc.
+}
+
+source $sourceProperties
+
+baseDL=/home/data/httpd/download.eclipse.org/cbi/updates/analyzers
+ideUpdate=${baseDL}/${updateRelease}/${buildId}
+#headlessUpdate=${baseDL}/headless/${updateRelease}/${buildId}
+
+deployRepos ${ideUpdate} ${build_home}/org.eclipse.cbi.p2repo.analyzers/releng/org.eclipse.cbi.p2repo.analyzers.repository/target/repository
+#deployRepos ${headlessUpdate} ${build_home}/org.eclipse.cbi.p2repo.aggregator/org.eclipse.cbi.p2repo.cli.product/target/repository
+
+# save away "data" from the build, as well as the deployable headless products
+cp ${build_home}/aggr/buildOutput.txt ${ideUpdate}
+cp ${sourceProperties} ${ideUpdate}
+cp ${propertiesfile} ${ideUpdate}
+cp ${phpProperties} ${ideUpdate}
+
+productroot=${build_home}/org.eclipse.cbi.p2repo.analyzers/releng/org.eclipse.cbi.p2repo.analyzers/target/products
+cp ${productroot}/org.eclipse.cbi.p2repo.analyzers.product-linux.gtk.x86_64.tar.gz  ${ideUpdate}/analyzers_${buildId}_linux.gtk.x86_64.tar.gz
+cp ${productroot}/org.eclipse.cbi.p2repo.analyzers.product-macosx.cocoa.x86_64.tar.gz ${ideUpdate}/analyzers_${buildId}_macosx.cocoa.x86_64.tar.gz
+cp ${productroot}/org.eclipse.cbi.p2repo.analyzers.product-win32.win32.x86_64.zip ${ideUpdate}/headless_${buildId}_win32.win32.x86_64.zip
+
+cp -r ${build_home}/p2repoSelfReport/reporeports ${ideUpdate}/
---
+++ releng/org.eclipse.cbi.p2repo.analyzers.parent/hudsonScripts/writeComposites.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+function writeArtifactsHeader
+{
+    outfile=$1
+    printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
+    printf "%s\n" "<?compositeArtifactRepository version='1.0.0'?>" >> ${outfile}
+    printf "%s\n" "<repository name='Eclipse CBI p2 Repository Aggregator'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>" >> ${outfile}
+    printf "%s\n" "  <properties size='3'>" >> ${outfile}
+    printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
+    printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
+    printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
+    printf "%s\n" "  </properties>" >> ${outfile}
+    printf "%s\n" "  <children size='3'>" >> ${outfile}
+
+}
+
+function writeContentHeader
+{
+    outfile=$1
+    printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
+    printf "%s\n" "<?compositeMetadataRepository version='1.0.0'?>" >> ${outfile}
+    printf "%s\n" "<repository name='Eclipse CBI p2 Repository Aggregator'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>" >> ${outfile}
+    printf "%s\n" "  <properties size='3'>" >> ${outfile}
+    printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
+    printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
+    printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
+    printf "%s\n" "  </properties>" >> ${outfile}
+    printf "%s\n" "  <children size='3'>" >> ${outfile}
+
+}
+
+function writeFooter
+{
+    outfile=$1
+    printf "%s\n" "  </children>" >> ${outfile}
+    printf "%s\n" "</repository>" >> ${outfile}
+}
+
+function writeCompositeP2Index
+{
+    printf "%s\n" "version=1" > "${p2Index}"
+    printf "%s\n" "metadata.repository.factory.order=compositeContent.xml" >> "${p2Index}"
+    printf "%s\n" "artifact.repository.factory.order=compositeArtifacts.xml" >> "${p2Index}"
+}
+
+function writeChildren
+{
+    outfile=$1
+    repoRoot=$2
+    # NOTE: we always take "most recent 3 builds". 
+    # we use "I20" as prefix that all our child repo directories start with 
+    # such as "I2016...". So, in 80 years will need some maintenance. :) 
+    # But, otherwise, this cheap heuristic finds existing files such as "composite*".
+    pushd "${repoRoot}" >/dev/null
+    children=$(ls -1td I20* | head -3)
+    popd >/dev/null
+
+    for child in $children
+    do
+        printf "%s%s%s\n" "    <child location='" $child "' />"  >> ${outfile}
+    done
+
+}
+
+repoRoots=("/home/data/httpd/download.eclipse.org/cbi/updates/aggregator/ide/4.6" "/home/data/httpd/download.eclipse.org/cbi/updates/aggregator/headless/4.6")
+# Normally "writeRepoRoots" is the same as "repoRoots", but might not always be, plus
+# it is very handy for testing this script not to have to write to the "production" area.
+#writeRepoRoots=("${PWD}/ide" "${PWD}/headless")
+writeRepoRoots=(${repoRoots[@]})
+indices=(0 1)
+for index in ${indices[@]} 
+do
+    #echo -e "[DEBUG] index: ${index}\n"
+    writeRepoRoot="${writeRepoRoots[$index]}"
+    #echo -e "[DEBUG] writeRepoRoot: ${writeRepoRoot}\n"
+    mkdir -p "${writeRepoRoot}"
+    RC=$?
+    if [[ $RC != 0 ]]
+    then
+       echo -e "[ERROR] Could not create directory at ${writeRepoRoot}\n"
+       exit $RC
+    fi
+    repoRoot="${repoRoots[$index]}"
+    #echo -e "[DEBUG] repoRoot: ${repoRoot}\n"
+    
+    artifactsCompositeFile="${writeRepoRoot}/compositeArtifacts.xml"
+    contentCompositeFile="${writeRepoRoot}/compositeContent.xml"
+    p2Index="${writeRepoRoot}/p2.index"
+
+    writeArtifactsHeader "${artifactsCompositeFile}"
+    writeChildren "${artifactsCompositeFile}" "${repoRoot}"
+    writeFooter "${artifactsCompositeFile}"
+
+    writeContentHeader "${contentCompositeFile}"
+    writeChildren "${contentCompositeFile}" "${repoRoot}"
+    writeFooter "${contentCompositeFile}"
+
+    writeCompositeP2Index "${p2Index}"
+done
+
