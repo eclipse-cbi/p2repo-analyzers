1c6e1f4a46d3ec9e3455027ea7890b93add6b89c
Александър Куртаков <akurtakov@gmail.com>
2023-05-17 10:17:21 +0300

    Remove LayoutChecker
    As pointed in https://github.com/eclipse-cbi/p2repo-analyzers/pull/37
    modigying LayoutChecker makes no difference as the actual executed code
    lies in TestLayoutTest(hooked in BuildRepoTests which itself is hooked
    in RepoReportApplication - default application for the product).

--- bundles/org.eclipse.cbi.p2repo.analyzers.common/src/org/eclipse/cbi/p2repo/analyzers/common/checker/CheckerRegistry.java
+++ bundles/org.eclipse.cbi.p2repo.analyzers.common/src/org/eclipse/cbi/p2repo/analyzers/common/checker/CheckerRegistry.java
@@ -16,7 +16,6 @@ import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.BREEChecker;
 import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.EclipseSourceChecker;
 import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.FeatureNameChecker;
 import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.IUNameChecker;
-import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.LayoutChecker;
 import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.LicenseConsistencyChecker;
 import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.ProviderNameChecker;
 import org.eclipse.cbi.p2repo.analyzers.common.checker.impl.SignatureChecker;
@@ -39,7 +38,6 @@ public class CheckerRegistry {
 		this.artifactCheckers.add(new BREEChecker());
 		this.artifactCheckers.add(new SignatureChecker());
 		this.artifactCheckers.add(new EclipseSourceChecker());
-		this.artifactCheckers.add(new LayoutChecker());
 		this.artifactCheckers = Collections.unmodifiableSet(this.artifactCheckers);
 	}
 
